4e70ad6f300e2a4c90034d043d94ad3338f44247
Naim <m-naimshah@dxg.jp>
2025-09-17 14:42:33 +0800

    Baqara content

--- sukun/index.js
+++ sukun/index.js
@@ -1,104 +1,133 @@
 const words = [
-    "أَرَءَيْتَ",
-    "فَأَبَوْا۟",
-    "بَيْنَهُمْ",
-    "عَلَيْكُمْ",
-    "بَيْنَكُمْ",
-    "وَبَيْنِكَ",
-    "عَلَيْهِمْ",
-    "وَخَيْرٌۭ",
-    "وَلَوْلَآ",
-    "عَلَيْكُم",
-    "عَلَيْهَا",
-    "مَوْئِلًا",
-    "عَلَيْهِم",
-    "عَيْنَاكَ",
-    "قَوْمُنَا",
-    "أَوَيْنَآ",
-    "بَيْنَنَا",
-    "بَيْنَهُم",
-    "إِلَيْكَ",
-    "قَوْمًۭا",
-    "لَدَيْهِ",
-    "عَلَيْهِ",
-    "فَسَوْفَ",
-    "عَلَيْكَ",
-    "بِغَيْرِ",
-    "خَيْرًۭا",
-    "وَكَيْفَ",
-    "وَيَوْمَ",
-    "شَيْـًٔا",
-    "وَخَيْرٌ",
-    "غَوْرًۭا",
-    "خَيْرٌۭ",
-    "بَيْنِى",
-    "قَوْمًا",
-    "قَوْمٍۢ",
-    "يَوْمٍۢ",
-    "يَوْمًا",
-    "قَوْلًا",
-    "بَيْنَ",
-    "خَيْرٌ",
-    "يَوْمَ",
-    "غَيْبُ",
-    "عَيْنٍ",
-    "وَلَوْ",
-    "رَيْبَ",
     "أَوْ",
-    "لَوْ"
+    "لَوْ",
+    "دُونِ",
+    "بَيْنَ",
+    "كَيْفَ",
+    "أَيْنَ",
+    "حَيْثُ",
+    "لَيْسَ",
+    "رَيْبَ",
+    "يَقِينَ",
+    "غَيْبِ",
+    "خَيْرٖ",
+    "خَيْرٌ",
+    "لَيْلَةٗ",
+    "لَيْلَةً",
+    "صَوْتٖ",
+    "شَيْءٖ",
+    "شَيْءٍ",
+    "خَوْفٌ",
+    "رَشِيدٖ",
+    "نَصِيرٖ",
+    "مَجِيدٖ",
+    "حَكِيمٖ",
+    "عَلِيمٖ",
+    "خَبِيرٖ",
+    "بَصِيرٖ",
+    "سَمِيعٖ",
+    "قَدِيرٖ",
+    "رَحِيمٖ",
+    "كَرِيمٖ",
+    "عَظِيمٖ",
+    "أَلِيمٖ",
+    "مُقِيمٖ",
+    "مُبِينٖ",
+    "حَمِيدٖ",
+    "يَوْمَ",
+    "يَوْمٖ",
+    "يَوْمِ",
+    "شَهِيدٖ",
+    "صَدِيقٖ",
+    "جَيْئًا",
+    "خَبِيثَ",
+    "غَيْرِ",
+    "خَيْرٗا",
+    "طَيْرِ",
+    "بَيْتِ",
+    "زَيْتُونٖ",
+    "مَيْسِرٖ",
+    "طَيِّبَٰتِ",
+    "أَيْدِيكُمْ",
+    "أَيْدِيهِمْ",
+    "بَيْتِيَ",
+    "مَيْتَةً",
+    "أَمَيْنٖ",
+    "أُولَـٰئِكَ",
+    "عَيْنٗا",
+    "صَيْبٖ",
+    "حَيَاةٖ",
+    "هَوَادَ",
+    "بَيْنَهُمْ",
+    "عِيسَى",
 ];
 
 const means = [
-    "have you seen?",
-    "but they refused",
-    "between them",
-    "upon you",
-    "between you",
-    "and between you",
-    "upon them",
-    "and it is better",
-    "if not for",
-    "upon you",
-    "upon her",
-    "any refuge",
-    "upon them",
-    "your two eyes",
-    "our people",
-    "we sought refuge",
-    "between us",
-    "between them",
-    "to you",
-    "a people",
-    "with him",
-    "upon him",
-    "then soon",
-    "upon you",
-    "without",
-    "good",
-    "and how",
-    "and the day",
-    "a thing",
-    "and better",
-    "sunk",
-    "better",
-    "between me",
-    "a people",
-    "a people",
-    "a day",
-    "a day",
-    "saying",
-    "between",
-    "good",
-    "day",
-    "the unseen",
-    "a spring",
-    "if only",
-    "doubt",
-    "or",
-    "if"
+    "or",                  // أَوْ
+    "if",                  // لَوْ
+    "without",             // دُونِ
+    "between",             // بَيْنَ
+    "how",                 // كَيْفَ
+    "where",               // أَيْنَ
+    "wherever",            // حَيْثُ
+    "is not",              // لَيْسَ
+    "doubt",               // رَيْبَ
+    "certainty",           // يَقِينَ
+    "hidden",              // غَيْبِ
+    "better",              // خَيْرٖ
+    "better",              // خَيْرٌ
+    "night",               // لَيْلَةٗ
+    "night",               // لَيْلَةً
+    "sound",               // صَوْتٖ
+    "thing",               // شَيْءٖ
+    "thing",               // شَيْءٍ
+    "fear",                // خَوْفٌ
+    "rightly guided",      // رَشِيدٖ
+    "supporter",           // نَصِيرٖ
+    "majestic",            // مَجِيدٖ
+    "wise",                // حَكِيمٖ
+    "all-knowing",         // عَلِيمٖ
+    "well-aware",          // خَبِيرٖ
+    "all-seeing",          // بَصِيرٖ
+    "all-hearing",         // سَمِيعٖ
+    "all-powerful",        // قَدِيرٖ
+    "merciful",            // رَحِيمٖ
+    "generous",            // كَرِيمٖ
+    "mighty",              // عَظِيمٖ
+    "painful",             // أَلِيمٖ
+    "establishing",        // مُقِيمٖ
+    "manifest",            // مُبِينٖ
+    "praiseworthy",        // حَمِيدٖ
+    "day",                 // يَوْمَ
+    "day",                 // يَوْمٖ
+    "day",                 // يَوْمِ
+    "witness",             // شَهِيدٖ
+    "sincere",             // صَدِيقٖ
+    "thing brought",       // جَيْئًا
+    "impure",              // خَبِيثَ
+    "different",           // غَيْرِ
+    "good",                // خَيْرٗا
+    "bird",                // طَيْرِ
+    "house",               // بَيْتِ
+    "olive",               // زَيْتُونٖ
+    "gambling",            // مَيْسِرٖ
+    "pure things",         // طَيِّبَٰتِ
+    "your hands",          // أَيْدِيكُمْ
+    "their hands",         // أَيْدِيهِمْ
+    "my house",            // بَيْتِيَ
+    "carrion",             // مَيْتَةً
+    "trustworthy",         // أَمَيْنٖ
+    "those",               // أُولَـٰئِكَ
+    "fountain",            // عَيْنٗا
+    "rain shower",         // صَيْبٖ
+    "life",                // حَيَاةٖ
+    "ease",                // هَوَادَ
+    "between them",        // بَيْنَهُمْ
+    "Isa",                 // عِيسَى
 ];
 
 const wordElm = document.querySelector('.word-text');
+const meanElm = document.querySelector('.mean-text');
 const progElm = document.querySelector('.prog-bar');
 const addBtnElm = document.querySelector('.add-button');
 const subBtnElm = document.querySelector('.sub-button');
